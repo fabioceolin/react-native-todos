1f5b5e665f7a46b9e8207ce31f968623a3128a40
Fábio Ceolin <fabioceolin98@gmail.com>
2021-05-25 21:47:00 -0300

    Desafio completo

--- src/components/MyTasksList.tsx
+++ src/components/MyTasksList.tsx
@@ -20,6 +20,7 @@ interface MyTasksListProps {
 }
 
 export function MyTasksList({ tasks, onLongPress, onPress }: MyTasksListProps) {
+
   return (
     <FlatList
       data={tasks}
@@ -29,14 +30,16 @@ export function MyTasksList({ tasks, onLongPress, onPress }: MyTasksListProps) {
           <TouchableOpacity
             testID={`button-${index}`}
             activeOpacity={0.7}
-            //TODO - use onPress, onLongPress and style props
+            onPress={() => onPress(item.id)} 
+            onLongPress={() => onLongPress(item.id)}
+            style={item.done ? styles.taskButtonDone : styles.taskButton}
           >
             <View 
               testID={`marker-${index}`}
-              //TODO - use style prop 
+              style={item.done ? styles.taskMarkerDone : styles.taskMarker}
             />
             <Text 
-              //TODO - use style prop
+              style={item.done ? styles.taskTextDone : styles.taskText}
             >
               {item.title}
             </Text>
